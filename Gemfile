ce487b63761a8888765aaae082d3391a8af23923
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-01-05 09:59:18 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+gem 'compass-rails'
+gem 'zurb-foundation'
+
+group :test do
+  gem 'capybara'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'thin'
+gem 'pg'
+gem 'debugger'
+gem 'devise'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeSandwich::Application.config.secret_token = '5d0504aae039231a909cb8990b6e747700aea03325494688719169e633073c258802482cab93b005dd8efafeb927947afa58f3a4bcaa697c34e848a2cdc29ff6'
