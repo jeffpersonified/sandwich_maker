d0f112448bfac908b9e289623ce6cbf6418f62b9
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-01-07 09:46:20 -0800

    fix single table inheritance bug caused by using 'type' as column name

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
+  self.inheritance_column = nil 
+  # fixes issue using 'type' as a column 
+  # name; 'type' invokes single table inheritance
+
   attr_accessible :instructions, :name, :options, :type
 
   validates :name, presence: true, length: { maximum: 45 }
